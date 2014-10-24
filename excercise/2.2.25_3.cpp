f81b06ec65817c36a2eac3b593f7e9563dc27783
youngyangH <huangyangyang200@sina.com>
2014-10-24 09:35:03 +0800

    2.2.25,3

--- excercise/2.2.25_3.cpp
+++ excercise/2.2.25_3.cpp
@@ -7,11 +7,30 @@ struct list{
 }
 
 
+/**
+  *
+  * 思路：把顺序表的后半段往前插值
+  *
+  *
+  */
 void merge3(list & a,int m,int n){
   int data;
-  int i=0;
-  while()
+  int i=0,j=a.length-1,k;
+  while(j>=m)
+    {
+      for(k=0;k<m;k++)
+      {
+        if(a[j]<a[k]){
+          for(i=j;i>=k;i++){
+            data=a[i];
+            a[i]=a[i-1];
+            a[i]=data;
 
+          }
+
+        }
+        j--;
+      }
   }
 
 }
