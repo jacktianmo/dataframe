b470eade590584ba9dd52d40d1fcfcd8fb801def
young <huangyangyang200@sina.com>
2014-11-07 22:21:40 +0800

    chapt5 is finished! just be yourself! Never Mind~~~ ^-^!

--- chapt5/src/main.cpp
+++ chapt5/src/main.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+/*
 void destroy(int m1,int m2);
 
 int main(){
@@ -18,7 +19,12 @@ int main(){
 
 	ptrobj2=new dynamicClass<int>(obj1);
 
+	obj1=*ptrobj2;
 	destroy(3,300);
+
+	int *j;
+	j=new int(1)	;
+	cout<<"show up:"<<*j<<endl;
 
 	return 0;
 }
@@ -27,4 +33,26 @@ void destroy(int m1,int m2){
 	dynamicClass<int>(m1,m2);
 
 }
+
+*/
+
+/*
+template<typename T>
+dynamicClass<int> demo(dynamicClass<T> one,dynamicClass<T>& two,const T& m);
+
+int main(){
+
+	dynamicClass<int> objA(3,5),objB=objA,objC(0,0);
+	objC=demo(objA,objB,5);
+
+	return 0;
+}
+
+
+template<typename T>
+dynamicClass<int> demo(dynamicClass<T> one, dynamicClass<T>& two,const T& m)
+{
+		dynamicClass<T>obj(m,m);
+		return obj;
+}*/
 
